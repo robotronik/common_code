2ea59dcde83d9c637bd5a833fe5bf48e3769d67d
Félix Piédallu <felix@piedallu.me>
2015-04-25 21:59:14 +0200

    avancé

--- uart/protocole.c
+++ uart/protocole.c
@@ -1,10 +1,46 @@
 
+char* temp_string;
+char* to_send_string;
 
-char* la_variable_que_l_uart_a_construit;
-
-void uart_new_xy_absolu(int x, int y) {
-    asprintf(la_variable_que_l_uart_a_construit, "x=%d\ny=%d\n", x, y);
-    asprintf(la_variable_que_l_uart_a_construit, "%s\n%s\n", la_variable_que_l_uart_a_construit, UART_KEYWORD_NEW_XY_ABSOLU);
-
+void uart_build_trame_new_xy_absolu(int x, int y) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n",
+             temp_string,
+             UART_KEYWORD_NEW_XY_ABSOLU);
+}
+void uart_build_trame_new_xy_relatif(int x, int y) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n",
+             temp_string,
+             UART_KEYWORD_NEW_XY_RELATIF);
 }
 
+void uart_build_trame_new_alpha_delta(int alpha, int delta) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
+}
+void uart_build__trame_new_xy_absolu(int x, int y) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
+}
+void uart_build_trame_new_xy_absolu(int x, int y) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
+}
+void uart_build_trame_new_xy_absolu(int x, int y) {
+    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
+    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
+}
+
+
+
+
+
+//appelées de l'extérieur uniquement pour les tests unitaires
+void new_alpha_delta(int alpha, int delta);
+void new_xy_relatif(int x, int y);
+void new_xy_absolu(int x, int y);
+void set_mode_tendu();
+void set_mode_courbe();
+void new_theta(int theta);
+void new_chemin();
