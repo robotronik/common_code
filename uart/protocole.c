5dee94d481ddb9380b43235fbd1d2ca96f367526
Félix Piédallu <felix@piedallu.me>
2015-04-26 02:21:27 +0200

    ajouté toutes les fonctions de création de trame

--- uart/protocole.c
+++ uart/protocole.c
@@ -1,46 +1,38 @@
+#include "protocole.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+char buffer_envoi[256];
 
-char* temp_string;
-char* to_send_string;
+#define add_to_buffer_with_args(format,...) sprintf(buffer_envoi, "%s" format "\n", buffer_envoi, __VA_ARGS__)
+#define add_to_buffer(string) sprintf(buffer_envoi, "%s%s\n", buffer_envoi, string)
 
-void uart_build_trame_new_xy_absolu(int x, int y) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n",
-             temp_string,
-             UART_KEYWORD_NEW_XY_ABSOLU);
+void uart_trame_builder_init() {
+    buffer_envoi[0] = '\0';
 }
-void uart_build_trame_new_xy_relatif(int x, int y) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n",
-             temp_string,
-             UART_KEYWORD_NEW_XY_RELATIF);
-}
-
-void uart_build_trame_new_alpha_delta(int alpha, int delta) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
-}
-void uart_build__trame_new_xy_absolu(int x, int y) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
-}
-void uart_build_trame_new_xy_absolu(int x, int y) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
-}
-void uart_build_trame_new_xy_absolu(int x, int y) {
-    asprintf(temp_string, "x=%d\ny=%d\n", x, y);
-    asprintf(to_send_string, "%s\n%s\n", temp_string, UART_KEYWORD_NEW_XY_ABSOLU);
+char* uart_trame_builder_get_string() {
+    return buffer_envoi;
 }
 
 
+void uart_trame_builder_append_envoie_coordonnees(int x, int y) {
+    add_to_buffer_with_args(UART_KEY_ENVOIE_X, x);
+    add_to_buffer_with_args(UART_KEY_ENVOIE_Y, y);
+}
 
+void uart_trame_builder_append_nouveau_point_dans_chemin()   { add_to_buffer(UART_KEY_POINT_DANS_CHEMIN);   }
+void uart_trame_builder_append_execute_le_chemin()           { add_to_buffer(UART_KEY_EXECUTE_LE_CHEMIN);   }
 
+void uart_trame_builder_append_mouvement_xy_absolu()         { add_to_buffer(UART_KEY_MV_XY_ABSOLU);        }
+void uart_trame_builder_append_mouvement_xy_relatif()        { add_to_buffer(UART_KEY_MV_XY_RELATIF);       }
 
-//appelées de l'extérieur uniquement pour les tests unitaires
-void new_alpha_delta(int alpha, int delta);
-void new_xy_relatif(int x, int y);
-void new_xy_absolu(int x, int y);
-void set_mode_tendu();
-void set_mode_courbe();
-void new_theta(int theta);
-void new_chemin();
+void uart_trame_builder_append_mouvement_alphadelta()        { add_to_buffer(UART_KEY_MV_ALPHADELTA);       }
+void uart_trame_builder_append_mouvement_alpha()             { add_to_buffer(UART_KEY_MV_ALPHA);            }
+void uart_trame_builder_append_mouvement_delta()             { add_to_buffer(UART_KEY_MV_DELTA);            }
+void uart_trame_builder_append_mouvement_theta()             { add_to_buffer(UART_KEY_MV_THETA);            }
+
+void uart_trame_builder_append_mode_tendu()                  { add_to_buffer(UART_KEY_MODE_TENDU);          }
+void uart_trame_builder_append_mode_courbe()                 { add_to_buffer(UART_KEY_MODE_COURBE);         }
+
+void uart_trame_builder_append_stop_normal()                 { add_to_buffer(UART_KEY_STOP_NORMAL);         }
+void uart_trame_builder_append_stop_urgence()                { add_to_buffer(UART_KEY_STOP_URGENCE);        }
--- uart/protocole.h
+++ uart/protocole.h
@@ -3,32 +3,48 @@
 /* PROTOCOLE DE TRANSMISSION TEXTE   */
 /* ================================= */
 
-#define uart_trame_xy(x,y) asprintf("x=%d\ny=%d\n", x, y)
+// Envoi de coordonnées
+#define UART_KEY_ENVOIE_X "x=%d"
+#define UART_KEY_ENVOIE_Y "y=%d"
+void uart_trame_builder_append_envoie_coordonnees(int x, int y);
+
+// Envoi d'un point au milieu d'un chemin
+#define UART_KEY_POINT_DANS_CHEMIN  "POINT_DANS_CHEMIN"
+void uart_trame_builder_append_nouveau_point_dans_chemin();
+#define UART_KEY_EXECUTE_LE_CHEMIN  "EXECUTE_LE_CHEMIN"
+void uart_trame_builder_append_execute_le_chemin();
+
+// Déplacements classiques
+void uart_trame_builder_append_mouvement_xy_absolu();
+#define UART_KEY_MV_XY_ABSOLU   "MV_XY_ABSOLU"
+#define UART_KEY_MV_XY_RELATIF  "MV_XY_RELATIF"
+void uart_trame_builder_append_mouvement_xy_relatif();
+
+// Tour sur soi-même, par exemple
+#define UART_KEY_MV_ALPHADELTA  "MV_ALPHADELTA"
+void uart_trame_builder_append_mouvement_alphadelta();
+#define UART_KEY_MV_ALPHA       "MV_ALPHA"
+void uart_trame_builder_append_mouvement_alpha();
+#define UART_KEY_MV_DELTA       "MV_DELTA"
+void uart_trame_builder_append_mouvement_delta();
+#define UART_KEY_MV_THETA       "MV_THETA"
+void uart_trame_builder_append_mouvement_theta();
+
+// Modes de déplacement
+#define UART_KEY_MODE_TENDU     "MODE_TENDU"
+void uart_trame_builder_append_mode_tendu();
+#define UART_KEY_MODE_COURBE    "MODE_COURBE"
+void uart_trame_builder_append_mode_courbe();
+
+#define UART_KEY_STOP_NORMAL    "STOP_NORMAL"
+void uart_trame_builder_append_stop_normal();
+#define UART_KEY_STOP_URGENCE   "STOP_URGENCE"
+void uart_trame_builder_append_stop_urgence();
 
 
-#define UART_KEYWORD_NEW_XY_ABSOLU "new_xy_absolu"
-#define uart_trame_new_xy_absolu(x, y) \
-    asprintf("%s\n%s\n", uart_trame_xy(x,y), UART_KEYWORD_NEW_XY_ABSOLU)
+//send_position_atteinte
 
+// Utilitaires : (ré)Initialisation du buffer, etc
+void uart_trame_builder_init();
+char* uart_trame_builder_get_string();
 
-
-
-
-#define uart_trame_new_xy_absolu(x, y) \
-    "%s\n%s\n", uart_trame_xy(x,y), UART_KEYWORD_NEW_XY_ABSOLU)
-
-
-
-
-
-
-
-
-//appelées de l'extérieur uniquement pour les tests unitaires
-void new_alpha_delta(int alpha, int delta);
-void new_xy_relatif(int x, int y);
-void new_xy_absolu(int x, int y);
-void set_mode_tendu();
-void set_mode_courbe();
-void new_theta(int theta);
-void new_chemin();
