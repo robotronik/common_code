ceec6f7810044147543ff80d1875b485ef284835
Félix Piédallu <felix@piedallu.me>
2015-04-26 13:31:50 +0200

    removed old symbols

--- simulation/affichage.c
+++ simulation/affichage.c
@@ -30,7 +30,7 @@ int init_sdl_screen() {
     // Texture : plateau de jeu
     texturePlateau = SOIL_load_OGL_texture(IMAGE_PLATEAU,
         SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-    debug_lvl3("SOIL messages : '%s' (%s)\n", SOIL_last_result(), IMAGE_PLATEAU);
+    debug(1, "SOIL messages : '%s' (%s)\n", SOIL_last_result(), IMAGE_PLATEAU);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glBindTexture(GL_TEXTURE_2D, texturePlateau);
@@ -44,7 +44,7 @@ int sdl_manage_events() {
             return 1;
         case SDL_MOUSEBUTTONDOWN: case SDL_MOUSEMOTION: // Clic de la souris
             if (evenements.button.button == SDL_BUTTON_LEFT) {
-                debug_lvl3("%d %d\n", evenements.button.x*ZOOM_FACTOR, PLATEAU_HEIGHT - evenements.button.y*ZOOM_FACTOR);
+                debug(3, "%d %d\n", evenements.button.x*ZOOM_FACTOR, PLATEAU_HEIGHT - evenements.button.y*ZOOM_FACTOR);
                 new_xy_absolu(evenements.button.x*ZOOM_FACTOR, PLATEAU_HEIGHT - evenements.button.y*ZOOM_FACTOR);
             }
         default:
@@ -105,7 +105,7 @@ void dessine_robot() {
 
 void bouge_robot_sdl(int x, int y, int alpha) {
     float alpha_deg = alpha * MRAD2DEGRES;
-    debug_lvl3("x = %d, y = %d, alpha = %f\n", x, y, alpha_deg);
+    debug(3, "x = %d, y = %d, alpha = %f\n", x, y, alpha_deg);
     // Remplissage de la surface avec du noir
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
