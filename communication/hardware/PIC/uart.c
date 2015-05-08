18ae2a294dd5daadeb5ba4d0a6bd5dd612f36565
Robin Moussu <robin.moussu+git@gmail.com>
2015-05-08 10:51:53 +0200

    Simplification de la fonction d'emission de l'assert

--- communication/hardware/PIC/uart.c
+++ communication/hardware/PIC/uart.c
@@ -6,9 +6,6 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#define INT_UART_TX
-#define TX_BUFFER_SIZE 64
-
 /**
  * Envoie un caractère dans l'UART
  */
@@ -18,24 +15,8 @@ static void UART_putc(unsigned char c);
 
 static void UART_putc(unsigned char c)
 {
-#ifdef INT_UART_TX
-    static unsigned char txBuffer[TX_BUFFER_SIZE];
-    static unsigned short txBufferLength = 0;
-    static unsigned short indexTxBuffer = 0;
-
-    if(txBufferLength || U1STAbits.UTXBF) {         // Si la file contient une donnée ou que le buffer du périphérique est plein
-        while (txBufferLength >= TX_BUFFER_SIZE);
-        IEC0bits.U1TXIE = 0;    // Désactivation de l'interruption pour modifier les variables globales
-        txBuffer[(indexTxBuffer + txBufferLength) % TX_BUFFER_SIZE] = c;
-        txBufferLength ++;
-        IEC0bits.U1TXIE = 1;    // Activation de l'interruption
-        return;
-    } else U1TXREG = c;
-
-#else //#ifdef INT_UART_TX
     while (U1STAbits.UTXBF);
         U1TXREG = c;
-#endif //#ifdef INT_UART_TX
 
     return;
 }
