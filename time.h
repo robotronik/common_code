353f2a58336b0b5ccd67667a4852f4576b497daa
Robin Moussu <robin.moussu+git@gmail.com>
2015-05-09 18:16:40 +0200

    Ajout de la libCommon
    Actuellement la libCommon ne contient que les fonctions relative à la
    gestion du temps, uniquement pour l'architecture PIC.

---
+++ time.h
@@ -0,0 +1,14 @@
+#ifndef TIME_H
+#define TIME_H
+
+/**
+ * Effectue une pause durant n milisecondes
+ */
+void pause_ms(unsigned short n);
+
+/**
+ * Effectue une pause durant n secondes
+ */
+void pause_s(unsigned short n);
+
+#endif /* TIME_H */
