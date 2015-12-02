613a9a79125020c83405b5ac595da93849112aa2
Félix Piédallu <felix@piedallu.me>
2015-12-02 20:22:46 +0100

    messages plus clairs

--- communication/a2s.c
+++ communication/a2s.c
@@ -4,8 +4,8 @@ char *a2s_keys[A2S_SIZE] = {
     [A2S_VAL_X] = "x=",
     [A2S_VAL_Y] = "y=",
     [A2S_VAL_THETA] = "theta=",
-    [A2S_CMD_DONE] = "d",
-    [A2S_CMD_SEND_POS] = "p",
-    [A2S_CMD_QUIT] = "q",
+    [A2S_CMD_DONE] = "done",
+    [A2S_CMD_SEND_POS] = "pos_sent",
+    [A2S_CMD_QUIT] = "quit",
     [A2S_CMD_HELP] = "?",
 };
